3f7bf62b03fda1f0c0724d25f5d03c157028311f
francismiko <1485059943@qq.com>
2024-09-10 02:47:16 +0800

    feat: tanstack router

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App.tsx";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
 import "./index.css";
+
+const router = createRouter({ routeTree });
 
 createRoot(document.getElementById("root") as HTMLElement).render(
 	<StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</StrictMode>,
 );
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), TanStackRouterVite()],
 });
