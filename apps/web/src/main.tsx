e721b339ebcefb388611a197a857dd6da98837b8
francismiko <1485059943@qq.com>
2024-09-10 03:30:09 +0800

    feat: reset styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
-import "./index.css";
+import "virtual:uno.css";
+import "@unocss/reset/sanitize/sanitize.css";
+import "@unocss/reset/sanitize/assets.css";
 
 const router = createRouter({ routeTree });
 
