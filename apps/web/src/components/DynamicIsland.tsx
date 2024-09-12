c28aaaa7ef4f1622134b915ed68283f931ae5801
francismiko <1485059943@qq.com>
2024-09-12 23:30:25 +0800

    feat: dynamic island

--- apps/web/src/components/DynamicIsland.tsx
+++ apps/web/src/components/DynamicIsland.tsx
@@ -2,8 +2,8 @@ import type { FC } from "react";
 
 export const DynamicIsland: FC = () => {
 	return (
-		<div className="flex w-full h-24 fixed justify-center items-center">
-			<div className="flex w-48 h-12 bg-black rounded-full hover:w-192">
+		<div className="flex w-full fixed justify-center mt-8">
+			<div className="flex w-48 h-8 relative bg-black rounded-full transition-all duration-400 delay-0 ease-in-out hover:w-192 hover:h-14 hover:delay-300 before:absolute before:content-[''] before:-inset-4 hover:before:-inset-8">
 				<div />
 			</div>
 		</div>
