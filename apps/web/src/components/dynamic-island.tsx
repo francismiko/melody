82e8d0ffff03b01677132f8c141b83f548db3e08
francismiko <1485059943@qq.com>
2024-10-10 03:05:05 +0800

    feat: timerRef

--- apps/web/src/components/dynamic-island.tsx
+++ apps/web/src/components/dynamic-island.tsx
@@ -1,23 +1,28 @@
 import { cn } from "@/libs/utils";
-import { debounce } from "radash";
-import { useCallback, useState, type FC } from "react";
+import { useRef, useState, type FC } from "react";
 
 export const DynamicIsland: FC = () => {
 	const [isHover, setIsHover] = useState<boolean>(false);
+	const timerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
 
-	const debouncedHover = useCallback(
-		debounce({ delay: 800 }, () => {
+	const handleMouseEnter = () => {
+		timerRef.current = setTimeout(() => {
 			setIsHover(true);
-		}),
-		[],
-	);
+		}, 700);
+	};
 
-	const handleMouseLeave = () => {};
+	const handleMouseLeave = () => {
+		if (timerRef.current) {
+			clearTimeout(timerRef.current);
+			timerRef.current = null;
+		}
+		setIsHover(false);
+	};
 
 	return (
 		<div className="flex w-full fixed justify-center top-8 z-50">
 			<div
-				onMouseEnter={debouncedHover}
+				onMouseEnter={handleMouseEnter}
 				onMouseLeave={handleMouseLeave}
 				className="flex w-48 h-8 p-1 relative place-content-between bg-black rounded-full transition-all duration-300 delay-0 ease-in-out hover:w-192 hover:h-14 hover:delay-500 before:absolute before:content-[''] before:-inset-4 hover:before:-inset-8 hover:px-8 hover:py-2"
 			>
