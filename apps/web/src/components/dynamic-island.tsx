619ec05714baac5b734e65d09982466e3006cf8d
francismiko <1485059943@qq.com>
2024-10-11 02:26:46 +0800

    feat: framer motion

--- apps/web/src/components/dynamic-island.tsx
+++ apps/web/src/components/dynamic-island.tsx
@@ -1,35 +1,67 @@
-import { cn } from "@/libs/utils";
-import { useRef, useState, type FC } from "react";
+import { useHover } from "@uidotdev/usehooks";
+import { AnimatePresence, motion } from "framer-motion";
+import type { FC } from "react";
 
 export const DynamicIsland: FC = () => {
-	const [isHover, setIsHover] = useState<boolean>(false);
-	const timerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
-
-	const handleMouseEnter = () => {
-		timerRef.current = setTimeout(() => {
-			setIsHover(true);
-		}, 700);
-	};
-
-	const handleMouseLeave = () => {
-		if (timerRef.current) {
-			clearTimeout(timerRef.current);
-			timerRef.current = null;
-		}
-		setIsHover(false);
-	};
+	const [hoverRef, isHovered] = useHover();
 
 	return (
-		<div className="flex w-full fixed justify-center top-8 z-50">
-			<div
-				onMouseEnter={handleMouseEnter}
-				onMouseLeave={handleMouseLeave}
-				className="flex w-48 h-8 p-1 relative place-content-between bg-black rounded-full transition-all duration-300 delay-0 ease-in-out hover:w-192 hover:h-14 hover:delay-500 before:absolute before:content-[''] before:-inset-4 hover:before:-inset-8 hover:px-8 hover:py-2"
+		<div className="flex w-full fixed justify-center top-8 z-99999">
+			<motion.div
+				ref={hoverRef}
+				initial={{ width: "10rem", height: "2rem" }}
+				whileHover={{ width: "36rem", height: "3rem" }}
+				whileTap={{ scale: 1.2 }}
+				transition={{ duration: 0.4 }}
+				className="flex relative bg-black rounded-full before:absolute before:content-[''] before:inset-0 hover:before:-inset-16"
 			>
-				<div className={cn(!isHover && "hidden", "bg-white w-16 h-full")} />
-				<div className={cn(!isHover && "hidden", "bg-white w-16 h-full")} />
-				<div className={cn(!isHover && "hidden", "bg-white w-16 h-full")} />
-			</div>
+				<AnimatePresence>
+					{isHovered && <ActivedBar />}
+				</AnimatePresence>
+        <AnimatePresence>
+          {!isHovered && <InactivedBar />}
+        </AnimatePresence>
+			</motion.div>
 		</div>
+	);
+};
+
+const ActivedBar: FC = () => {
+	return (
+		<motion.div
+			key="actived"
+			initial={{ opacity: 0, y: -40 }}
+			exit={{ opacity: 0, y: 40 }}
+			animate={{ opacity: 1, y: 0 }}
+			transition={{ duration: 0.4 }}
+			className="flex w-full h-full place-content-between"
+		>
+			<div className="text-white flex flex-1 flex-row items-center">
+				<div>LOGO</div>
+			</div>
+			<div className="text-white flex flex-1 justify-center items-center">
+				<div>选项</div>
+			</div>
+			<div className="text-white flex flex-1 flex-row-reverse items-center">
+				<div>设置</div>
+			</div>
+		</motion.div>
+	);
+};
+
+const InactivedBar: FC = () => {
+	return (
+		<motion.div
+			key="inactived"
+			initial={{ opacity: 0, y: -40 }}
+			exit={{ opacity: 0, y: 40 }}
+			animate={{ opacity: 1, y: 0 }}
+			transition={{ duration: 0.4 }}
+			className="flex absolute w-full h-full"
+		>
+			<div className="text-white flex flex-1 justify-center items-center transition-all">
+				<div>信息展示</div>
+			</div>
+		</motion.div>
 	);
 };
