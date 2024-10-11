cb9d63c75708bf3d27b6fcee7ab1ba59d34f39b4
francismiko <1485059943@qq.com>
2024-10-12 02:09:48 +0800

    feat: animation encapsulation

--- apps/web/src/components/dynamic-island.tsx
+++ apps/web/src/components/dynamic-island.tsx
@@ -2,6 +2,8 @@ import { useHover } from "@uidotdev/usehooks";
 import { AnimatePresence, motion } from "framer-motion";
 import type { FC } from "react";
 
+const _duration_ = 0.5;
+
 export const DynamicIsland: FC = () => {
 	const [hoverRef, isHovered] = useHover();
 
@@ -12,8 +14,8 @@ export const DynamicIsland: FC = () => {
 				initial={{ width: "10rem", height: "2.4rem" }}
 				whileHover={{ width: "36rem", height: "3.2rem" }}
 				whileTap={{ scale: 1.2 }}
-				transition={{ duration: 0.4 }}
-				className="flex relative bg-black rounded-full before:absolute before:content-[''] before:-inset-0 hover:before:-inset-16"
+				transition={{ duration: _duration_ }}
+				className="flex relative overflow-hidden bg-black rounded-full before:absolute before:content-[''] before:-inset-0 hover:before:-inset-16"
 			>
 				<AnimatePresence>
 					{isHovered ? (
@@ -28,47 +30,71 @@ export const DynamicIsland: FC = () => {
 };
 
 const ActivedBar: FC = () => {
+	const sideBarAnimation = (direction: "left" | "right") => ({
+		initial: {
+			opacity: 0,
+			scale: 0.4,
+			x: (direction === "left" ? -1 : 1) * 200,
+		},
+		exit: {
+			opacity: 0,
+			scale: 0.4,
+			x: (direction === "left" ? -1 : 1) * 100,
+		},
+		animate: { opacity: 1, scale: 1, x: 0 },
+		transition: { duration: _duration_ },
+	});
+
+	const middleBarAnimation = {
+		initial: { opacity: 0, scale: 1, y: -50 },
+		exit: { opacity: 0, scale: 1, y: 50 },
+		animate: { opacity: 1, scale: 1, y: 0 },
+		transition: { duration: _duration_ },
+	};
+	const LeftBar: FC = () => (
+		<motion.div
+			{...sideBarAnimation("left")}
+			className="text-white flex flex-1 flex-row items-center gap-4"
+		>
+			{[...Array(2)].map((_, i) => (
+				<div key={i} className="w-8">
+					选项
+				</div>
+			))}
+		</motion.div>
+	);
+
+	const RightBar: FC = () => (
+		<motion.div
+			{...sideBarAnimation("right")}
+			className="text-white flex flex-1 flex-row-reverse items-center gap-4"
+		>
+			{[...Array(2)].map((_, i) => (
+				<div key={i} className="w-8">
+					选项
+				</div>
+			))}
+		</motion.div>
+	);
+
+	const MiddleBar: FC = () => (
+		<motion.div
+			{...middleBarAnimation}
+			className="text-white flex flex-1 justify-center items-center gap-4"
+		>
+			{[...Array(3)].map((_, i) => (
+				<div key={i} className="w-8">
+					选项
+				</div>
+			))}
+		</motion.div>
+	);
+
 	return (
 		<div className="flex absolute w-full h-full place-content-between">
-			<motion.div
-				initial={{ opacity: 0, scale: 0.4, x: -200 }}
-				exit={{ opacity: 0, scale: 0.4, x: -100 }}
-				animate={{ opacity: 1, scale: 1, x: 0 }}
-				transition={{ duration: 0.4 }}
-				className="text-red flex flex-1 flex-row items-center gap-4"
-			>
-				{[...Array(2)].map((_, i) => (
-					<div key={i} className="w-8">
-						选项
-					</div>
-				))}
-			</motion.div>
-			<motion.div
-				initial={{ opacity: 0, scale: 1, y: -50 }}
-				exit={{ opacity: 0, scale: 1, y: 50 }}
-				animate={{ opacity: 1, scale: 1, y: 0 }}
-				transition={{ duration: 0.4 }}
-				className="text-red flex flex-1 justify-center items-center gap-4"
-			>
-				{[...Array(3)].map((_, i) => (
-					<div key={i} className="w-8">
-						选项
-					</div>
-				))}
-			</motion.div>
-			<motion.div
-				initial={{ opacity: 0, scale: 0.4, x: 200 }}
-				exit={{ opacity: 0, scale: 0.4, x: 100 }}
-				animate={{ opacity: 1, scale: 1, x: 0 }}
-				transition={{ duration: 0.4 }}
-				className="text-red flex flex-1 flex-row-reverse items-center gap-4"
-			>
-				{[...Array(2)].map((_, i) => (
-					<div key={i} className="w-8">
-						选项
-					</div>
-				))}
-			</motion.div>
+			<LeftBar />
+			<MiddleBar />
+			<RightBar />
 		</div>
 	);
 };
@@ -79,10 +105,10 @@ const InactivedBar: FC = () => {
 			initial={{ opacity: 0, y: -50 }}
 			exit={{ opacity: 0, y: 50 }}
 			animate={{ opacity: 1, y: 0 }}
-			transition={{ duration: 0.4 }}
+			transition={{ duration: _duration_ }}
 			className="flex absolute w-full h-full"
 		>
-			<div className="text-red flex flex-1 justify-center items-center transition-all">
+			<div className="text-white flex flex-1 justify-center items-center">
 				<div>信息展示</div>
 			</div>
 		</motion.div>
