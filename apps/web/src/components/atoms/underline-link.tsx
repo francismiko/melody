230c532e6fc0ab13ecd20525070f68ffa4c0dff5
francismiko <1485059943@qq.com>
2024-11-06 22:23:30 +0800

    feat: cursor

--- apps/web/src/components/atoms/underline-link.tsx
+++ apps/web/src/components/atoms/underline-link.tsx
@@ -1,35 +1,41 @@
 import { cn } from "@/lib/utils";
 import { useHover } from "@uidotdev/usehooks";
 import { motion } from "framer-motion";
-import type { CSSProperties } from "react";
+import type { CSSProperties, FC } from "react";
 
 interface UnderlineLinkProps {
 	children: React.ReactNode;
 	underlineColor?: string;
 	className?: string;
+	onClick?: () => void;
 }
 
-export const UnderlineLink = ({
+export const UnderlineLink: FC<UnderlineLinkProps> = ({
 	children,
 	underlineColor = "#ffffff",
 	className,
-}: UnderlineLinkProps) => {
+	onClick,
+}) => {
 	const [hoverRef, isHovered] = useHover();
 
 	return (
-		<span ref={hoverRef} className="relative inline-block">
+		<span
+			ref={hoverRef}
+			onClick={onClick}
+			className="relative inline-block cursor-pointer"
+		>
 			{children}
 			<motion.span
 				style={{ "--underline-color": underlineColor } as CSSProperties}
-				initial={{ scaleX: 0, transformOrigin: "left" }}
+				initial={{ transformOrigin: "left" }}
 				animate={{
 					scaleX: isHovered ? 1 : 0,
 					transformOrigin: isHovered ? "left" : "right",
-					opacity: isHovered ? 1 : 0.5,
+					opacity: isHovered ? 1 : 0.6,
 				}}
-				transition={{ duration: 0.25, ease: "easeInOut" }}
+				transition={{ delay: 0.04, duration: 0.25, ease: "easeInOut" }}
 				className={cn(
-					"absolute bottom-0 left-0 right-0 rounded-full h-[1.5px] bg-[--underline-color] underline",
+					"absolute bottom-0 left-0 right-0 rounded h-[1.5px] bg-[--underline-color]",
 					className,
 				)}
 			/>
