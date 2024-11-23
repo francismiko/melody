00d514599db6a8e5d46c1b30b169dd81ff181d75
francismiko <1485059943@qq.com>
2024-11-23 21:25:26 +0800

    feat: natural shadow

--- apps/web/src/routes/blog/index.tsx
+++ apps/web/src/routes/blog/index.tsx
@@ -1,5 +1,39 @@
-import { createFileRoute } from '@tanstack/react-router'
+import DotPattern from "@/components/ui/dot-pattern";
+import { cn } from "@/lib/utils";
+import { createFileRoute } from "@tanstack/react-router";
+import type { FC } from "react";
 
-export const Route = createFileRoute('/blog/')({
-  component: () => <div>Hello /blog/!</div>
-})
+type BlogMetadataType = {
+	title: string;
+	author: string;
+	description: string;
+	date: string;
+	background: string;
+};
+
+const Index: FC = () => {
+	const blogMetadata: BlogMetadataType = [
+		{
+			title: "Title",
+			author: "Author",
+			description: "Description",
+			date: "Date",
+			background: "Background",
+		},
+	];
+
+	return (
+		<div className="flex flex-col items-center pt-24 pb-8">
+			<div className="w-7xl min-h-[80vh] bg-[#ffffff]/70 backdrop-blur-sm rounded-2xl shadow-natural-1 dark:bg-[#e0c2ff33] dark:shadow-custom-dark" />
+			<DotPattern
+				className={cn(
+					"[mask-image:radial-gradient(100vmin_circle_at_center,white,transparent)] fixed z-[-1]",
+				)}
+			/>
+		</div>
+	);
+};
+
+export const Route = createFileRoute("/blog/")({
+	component: Index,
+});
